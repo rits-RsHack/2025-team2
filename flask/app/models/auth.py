2bf33288e5b7f59d0cb4aece0b3aad14b7edad29
Kamakou <koukouvn@gmail.com>
2025-09-12 22:30:35 +0900

    fix データベースからクラス定義

--- flask/app/models/auth.py
+++ flask/app/models/auth.py
@@ -1,14 +1,21 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User:
-    def __init__(self,password,name,mail):
-        self.password_hash=generate_password_hash(password)
-        self.name=name
-        self.mail=mail
+# SQLAlchemyのBaseクラスを定義
+# このクラスを継承してモデルを作成します
+Base = declarative_base()
 
-    def check_password(self,password):
-        return check_password_hash(self.password_hash,password)
-        
+class User(Base):
+    __tablename__ = 'users'  # テーブル名を定義
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    mail = Column(String(100), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+
+    def __repr__(self):
+        # オブジェクトを文字列で表現するためのメソッド
+        return f"<User(name='{self.name}', mail='{self.mail}')>"
 
 
